1a1e140003b28de562cd56d24ec9a75147460718
Parrot User <user@localhost.localdomain>
2021-07-02 13:54:19 +0100

    test sockets

---
+++ socket/client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <Game />
+    </div>
+</template>
+
+<script>
+import Game from './components/Game.vue'
+
+export default {
+	name: 'app',
+	components: {
+		Game
+	}
+}
+</script>
+
+<style>
+    body {
+        background-color: #ddd;
+    }
+</style>
---
+++ socket/serv/app.js
@@ -0,0 +1,41 @@
+const Express = require("express")();
+const Http = require("http").Server(Express);
+const Socketio = require("socket.io")(Http);
+
+var position = {
+    x: 200,
+    y: 200
+};
+
+var position = {
+    x: 200,
+    y: 200
+};
+
+Socketio.on("connection", socket => {
+    socket.emit("position", position);
+    socket.on("move", data => {
+        switch(data) {
+            case "left":
+                position.x -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "right":
+                position.x += 5;
+                Socketio.emit("position", position);
+                break;
+            case "up":
+                position.y -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "down":
+                position.y += 5;
+                Socketio.emit("position", position);
+                break;
+        }
+    });
+});
+
+Http.listen(3000, () => {
+    console.log("Listening at :3000...");
+});
