e570ebcfd95a5b2de927f2e0a619d221634f6c14
Nico <arrvac@gmail.com>
2021-07-19 14:52:54 +0200

    chat private

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -56,7 +56,7 @@
 										</div>
 										<div class="my-3 p-3 bg-body rounded shadow-sm">
 											<h6 class="border-bottom pb-2 mb-0">Resulat de la recherche</h6>
-											<div class="d-flex text-muted pt-3" v-for="(friend, index) of filterFriends" :key="index">
+											<div class="d-flex text-muted pt-3" v-for="(friend, index) of filterFriends" :key="index" v-on:click="createPrivateChat(friend)">
 												<svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em"></text></svg>
 												<p class="pb-3 mb-0 small lh-sm border-bottom">
 													<strong class="d-block text-gray-dark">{{friend.login}}</strong>
@@ -171,6 +171,14 @@
 			},
 		},
 		methods: {
+			createPrivateChat(User)
+			{
+				const data = {
+					login: this.user.login,
+					user: User.login
+				}
+				this.socket.emit('createPrivateChat', data);
+			},
 			createGroupChat() {
 				const data = {
 					login: this.user.login,
